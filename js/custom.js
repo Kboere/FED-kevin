ed4aa78a7b0af27738684a7582cc3480c9cd4d52
Kboere <112857604+Kboere@users.noreply.github.com>
2022-09-30 12:41:49 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,6 @@ var navMenu = document.querySelector(" header nav ul");
 var navbar = document.querySelector(" header nav");
 
 hamburger.addEventListener("click", mobileMenu);
-console.log(hamburger);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
@@ -61,39 +60,4 @@ const myObserver = new IntersectionObserver(callback, options)
 svgs.forEach(svg => {
     myObserver.observe(svg)
 })
-
-
-/* Slideshow Manual */
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-
-
 
